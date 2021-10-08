f91f8a2fba55b9ba80b770ee6b70959792be974f
Alan Jagar <alan.jagar@talentlyft.com>
2021-10-08 13:55:15 +0200

    Add project files.

---
+++ Conversation.Api1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Conversation.Api1.Controllers
+{
+    [ApiController]
+    [Route("home")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<string> Get([FromServices] DaprClient daprClient)
+        {
+            await daprClient.SaveStateAsync("conversation.api1", "pero", "alan");
+
+            var value = daprClient.GetStateAsync<string>("conversation.api1", "pero");
+
+            return "Dominik: " + value;
+        }
+    }
+}
