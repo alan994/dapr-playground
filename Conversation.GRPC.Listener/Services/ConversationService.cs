4cd2e2bd40306d3beace2c0bf5c2e116cb9d6cef
Alan Jagar <alan.jagar@gmail.com>
2021-10-12 06:35:15 +0200

    svasta nesto oko grpc-a

--- Conversation.GRPC.Listener/Services/ConversationService.cs
+++ Conversation.GRPC.Listener/Services/ConversationService.cs
@@ -1,6 +1,7 @@
 ﻿using Conversation.shared;
 using Dapr.AppCallback.Autogen.Grpc.v1;
 using Dapr.Client;
+using Dapr.Client.Autogen.Grpc.v1;
 using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
@@ -8,6 +9,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 using static Dapr.AppCallback.Autogen.Grpc.v1.AppCallback;
@@ -46,6 +48,24 @@ namespace Conversation.GRPC.Listener.Services
             return Task.FromResult(result);
         }
 
+        public override async Task<InvokeResponse> OnInvoke(InvokeRequest request, ServerCallContext context)
+        {
+            var response = new InvokeResponse();
+            switch (request.Method)
+            {
+                case "mail":     
+                    var message = request.Data.Unpack<Conversation.GRPC.Generated.Message>();
+                    await Email(message);
+                    response.Data = Any.Pack(new Conversation.GRPC.Generated.Empty());
+                    break;
+                case "sms":
+                    var input = request.Data.Unpack<Conversation.GRPC.Generated.Message>();
+                    await Email(input);
+                    response.Data = Any.Pack(new Conversation.GRPC.Generated.Empty());
+                    break;
+            }
+            return response;
+        }
 
         /// <summary>
         /// implement OnTopicEvent to handle deposit and withdraw event
@@ -58,29 +78,35 @@ namespace Conversation.GRPC.Listener.Services
         {
             if (request.PubsubName == "conversation-pubsub")
             {
-                var input = JsonConvert.DeserializeObject<Message>(request.Data.ToStringUtf8());
+                var input = JsonConvert.DeserializeObject<Generated.Message>(request.Data.ToStringUtf8());
                                 
                 if (request.Topic == "mail")
                 {
-                    Email(input);
+                    await Email(input);
                 }
                 else if(request.Topic == "sms")
                 {
-                    Sms(input);
+                    await Sms(input);
                 }
             }
 
             return new TopicEventResponse();
         }
 
-        private void Email(Message input)
+        private async Task Email(Generated.Message input)
         {
-            this._logger.LogInformation("Email message received: ", JsonConvert.SerializeObject(input));
+            this._logger.LogInformation("Email message received: {0}", JsonConvert.SerializeObject(input));
+
+            var httpClient = DaprClient.CreateInvokeHttpClient();
+            var response = await httpClient.GetAsync("http://conversation-api1/home");
+            this._logger.LogInformation("Result from service invocation {0}", JsonConvert.SerializeObject(await response.Content.ReadAsStringAsync()));
         }
 
-        private void Sms(Message input)
+        private async Task Sms(Generated.Message input)
         {
             this._logger.LogInformation("SMS message received: ", JsonConvert.SerializeObject(input));
+            //var response = await this._daprClient.InvokeMethodGrpcAsync<Service.HelloRequest, Service.HelloReply>("conversation-grpc-service", "SayHello", new Service.HelloRequest() { Name = "Alan SMS" });
+            //this._logger.LogInformation("SMS message received by GRPC service invocation: ", JsonConvert.SerializeObject(response));
         }
     }
 }
